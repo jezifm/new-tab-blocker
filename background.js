ceeacf9f522ddb6ca9cc68f95204148f3ca0eacf
Jezrael Arciaga <jezarciaga@gmail.com>
2019-03-12 21:13:28 +0800

    Remove checking of grandparent tab

--- background.js
+++ background.js
@@ -10,15 +10,6 @@ chrome.tabs.onCreated.addListener(function(tab) {
 
       if (isBlacklist(tabParent.url, bad_domains)) {
         chrome.tabs.remove(tab.id, handleError);
-      } else if (tabParent.openerTabId) {
-        // is grandparent blacklist
-        chrome.tabs.get(tabParent.openerTabId, function(tabGrandParent) {
-          if (tabGrandParent.url === "chrome://newtab/") return;
-
-          if (isBlacklist(tabGrandParent.url, bad_domains)) {
-            chrome.tabs.remove(tab.id, handleError);
-          }
-        });
       }
     });
   });
