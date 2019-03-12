e6bd0a27311f4a43a4a940349cb172c46f5fef4f
Jezrael Arciaga <jezarciaga@gmail.com>
2019-03-12 21:00:30 +0800

    Move tabs.onCreate outside onInstalled event

--- background.js
+++ background.js
@@ -3,31 +3,28 @@ function isBlacklist(url, bad_domains) {
   return bad_domains.includes(host);
 }
 
-chrome.runtime.onInstalled.addListener(function() {
+chrome.tabs.onCreated.addListener(function(tab) {
+  if (tab.url === "chrome://newtab/") return;
 
-  chrome.tabs.onCreated.addListener(function(tab) {
-    if (tab.url === "chrome://newtab/") return;
+  chrome.storage.sync.get({bad_domains: []}, function(data) {
+    let bad_domains = data.bad_domains;
 
-    chrome.storage.sync.get({bad_domains: []}, function(data) {
-      let bad_domains = data.bad_domains;
+    // is parent blacklist
+    chrome.tabs.get(tab.openerTabId, function(tabParent) {
+      if (tabParent.url === "chrome://newtab/") return;
 
-      // is parent blacklist
-      chrome.tabs.get(tab.openerTabId, function(tabParent) {
-        if (tabParent.url === "chrome://newtab/") return;
+      if (isBlacklist(tabParent.url, bad_domains)) {
+        chrome.tabs.remove(tab.id, handleError);
+      } else if (tabParent.openerTabId) {
+        // is grandparent blacklist
+        chrome.tabs.get(tabParent.openerTabId, function(tabGrandParent) {
+          if (tabGrandParent.url === "chrome://newtab/") return;
 
-        if (isBlacklist(tabParent.url, bad_domains)) {
-          chrome.tabs.remove(tab.id, handleError);
-        } else if (tabParent.openerTabId) {
-          // is grandparent blacklist
-          chrome.tabs.get(tabParent.openerTabId, function(tabGrandParent) {
-            if (tabGrandParent.url === "chrome://newtab/") return;
-
-            if (isBlacklist(tabGrandParent.url, bad_domains)) {
-              chrome.tabs.remove(tab.id, handleError);
-            }
-          });
-        }
-      });
+          if (isBlacklist(tabGrandParent.url, bad_domains)) {
+            chrome.tabs.remove(tab.id, handleError);
+          }
+        });
+      }
     });
   });
 });
