be453986360a02f21746aceb980ad93d7078dfd9
Jezrael Arciaga <jezarciaga@gmail.com>
2019-03-12 21:49:58 +0800

    Swap tab query and storage logic

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 chrome.tabs.onCreated.addListener(function(tab) {
   if (tab.url === "chrome://newtab/") return;
 
-  chrome.storage.sync.get({bad_domains: []}, function(data) {
-    let bad_domains = data.bad_domains;
+  // is parent blacklist
+  chrome.tabs.get(tab.openerTabId, function(tabParent) {
+    if (tabParent.url === "chrome://newtab/") return;
 
-    // is parent blacklist
-    chrome.tabs.get(tab.openerTabId, function(tabParent) {
-      if (tabParent.url === "chrome://newtab/") return;
+    chrome.storage.sync.get({bad_domains: []}, function(data) {
+      let bad_domains = data.bad_domains;
 
       if (isBlacklist(tabParent.url, bad_domains)) {
         chrome.tabs.remove(tab.id, handleError);
